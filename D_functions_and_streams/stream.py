ebe8d41ed0cec6e4a3da96888442fc2df22dbc05
Sven Graupner <sgraupner@bht-berlin.de>
2022-11-03 20:59:14 +0100

    moved constructors to begin of class

--- D_functions_and_streams/stream.py
+++ D_functions_and_streams/stream.py
@@ -1,29 +1,37 @@
 import random
-from functools import cmp_to_key
 
+"""
+Class of a data stream comprised of a sequence of stream operations:
+    - slice(i1, i2, i3)    # slice stream in analogy to python slicing
+    - filter(filter_func)  # pass only elements for which filter_func yields True
+    - map(map_func)        # pass stream where each element is mapped by map_func
+    - sort(comperator_func) # pass stream sorted by comperator_func
+    - cond(cond, cond_func) # pass stream or apply conditional function
+    - print()              # pass unchanged stream and print as side effect
 
+and with terminal functions:
+    - reduce(reduce_func, start)   # compound stream to single value with reduce_func
+    - count()              # return number of elements in terminal stream
+    - get()                # return final stream data
+"""
 class Stream:
-    """
-    Class of a data stream comprised of a sequence of stream operations:
-     - slice(i1, i2, i3)    # slice stream in analogy to python slicing
-     - filter(filter_func)  # pass only elements for which filter_func yields True
-     - map(map_func)        # pass stream where each element is mapped by map_func
-     - sort(comperator_func) # pass stream sorted by comperator_func
-     - cond(cond, cond_func) # pass stream or apply conditional function
-     - print()              # pass unchanged stream and print as side effect
 
-    and with terminal functions:
-     - reduce(reduce_func, start)   # compound stream to single value with reduce_func
-     - count()              # return number of elements in terminal stream
-     - get()                # return final stream data
-    """
+    def __init__(self, _data=[]):
+        # constructor to initialize instance member variables
+        #
+        self.__streamSource = self.__new_op(_data)
+
 
     class __Stream_op:
         """
         Inner class of one stream operation with chainable functions.
         Instances comprise the stream pipeline.
         """
-        
+        def __init__(self, _new_op_func, _data):
+            self.__data = _data
+            self.__new = _new_op_func    # __new_op() function injected from outer context
+
+
         def slice(self, i1, i2=None, i3=1):
             # function that returns new __Stream_op instance that slices stream
             if i2 == None:
@@ -105,11 +113,6 @@ class Stream:
             return self.__data
 
 
-        def __init__(self, _new_op_func, _data):
-            self.__data = _data
-            self.__new = _new_op_func    # __new_op() function injected from outer context
-
-
     def source(self):
         # return first __Stream_op instance of stream as source
         #
@@ -121,56 +124,127 @@ class Stream:
         return Stream.__Stream_op(self.__new_op, *argv)
 
 
-    def __init__(self, _data=[]):
-        # constructor to initialize instance member variables
-        #
-        self.__streamSource = self.__new_op(_data)
-
-
-names = ['Gonzalez', 'Gill', 'Hardin', 'Richardson', 'Buckner', 'Marquez',
-    'Howe', 'Ray', 'Navarro', 'Talley', 'Bernard', 'Gomez', 'Hamilton',
-    'Case', 'Petty', 'Lott', 'Casey', 'Hall', 'Pena', 'Witt', 'Joyner',
-    'Raymond', 'Crane', 'Hendricks', 'Vance', 'Cleveland', 'Duncan', 'Soto',
-    'Brock', 'Graham', 'Nielsen', 'Rutledge', 'Strong', 'Cox']
-
-
+# Ignore this code that loads solution from file, if exists.
+# The solution is not distributed.
 try:
-    # overload main class with solution class from (package, path), if exists
-    ovl = ('stream_sol', 'Stream')
-    Stream = getattr(__import__(ovl[0], fromlist=[ovl[1]]), ovl[1])
+    _from, _import = 'stream_sol', 'Stream'
+    Stream = getattr(__import__(_from, fromlist=[_import]), _import)
+#
 except ImportError:
     pass
 
 
 if __name__ == '__main__':
 
-    # Challenge 1, Data streams in Python, run the first example
-    result = Stream(names).source() \
-        .filter(lambda n : len(n) == 4) \
-        .print() \
-        .count()
+    run_choice = 1
     #
-    print(f'found {result} names with 4 letters.')
+    run_choices = {
+        1:  "Challenge 1, Data streams in Python, run the first example",
+        2:  "Challenge 2, complete map() function",
+        3:  "Challenge 3, complete reduce() function",
+        31: "Challenge 3.1, example RAYCOX",
+        4:  "Challenge 4, complete sort() function",
+        41: "Challenge 4.1, len-alpha comperator",
+        42: "Challenge 4.2, tuple output: ('Cox', 'Xoc', 3)",
+        5:  "Challenge 5, Pipeline for product codes",
+        51: "Challenge 5.1, even digit codes"
+    }
 
-    # Challenge 2, complete map() function
-    # to map names to name lengths for the first 8 names
-    Stream(names).source() \
-        .slice(8) \
-        .print() \
-        .map(lambda n : len(n)) \
-        .print()
+    names = ['Gonzalez', 'Gill', 'Hardin', 'Richardson', 'Buckner', 'Marquez',
+        'Howe', 'Ray', 'Navarro', 'Talley', 'Bernard', 'Gomez', 'Hamilton',
+        'Case', 'Petty', 'Lott', 'Casey', 'Hall', 'Pena', 'Witt', 'Joyner',
+        'Raymond', 'Crane', 'Hendricks', 'Vance', 'Cleveland', 'Duncan', 'Soto',
+        'Brock', 'Graham', 'Nielsen', 'Rutledge', 'Strong', 'Cox']
 
 
-    # Challenge 3, complete reduce() function
+    if run_choice == 1:
+        # Challenge 1, Data streams in Python, run the first example
+        result = Stream(names).source() \
+            .filter(lambda n : len(n) == 4) \
+            .print() \
+            .count()
+        #
+        print(f'found {result} names with 4 letters.')
 
-    # Challenge 3.1, example RAYCOX, compound single string of all n-letter names
+    if run_choice == 2:
+        # Challenge 2, complete map() function
+        # to map names to name lengths for the first 8 names
+        Stream(names).source() \
+            .slice(8) \
+            .print() \
+            .map(lambda n : len(n)) \
+            .print()
 
-    # Challenge 4, complete sort() function
+    if run_choice == 3:
+        # Challenge 3, complete reduce() function
+        # to compound all name lengths to a single result
+        result = Stream(names).source() \
+            .slice(8) \
+            .print() \
+            .map(lambda n : len(n)) \
+            .print() \
+            .reduce(lambda x, y : x + y)
+        #
+        print(f'compound number of letters in names is: {result}.')
 
-    # Challenge 4.1, len-alpha comperator
+    if run_choice == 31:
+        # Challenge 3.1, example RAYCOX
+        # compound single string of all n-letter names
+        n = 3
+        result = Stream(names).source() \
+            .filter(lambda name : len(name) == n) \
+            .print() \
+            .map(lambda n : n.upper()) \
+            .reduce(lambda x, y : str(x) + str(y), '')
+        #
+        print(f'compounded {n}-letter names: {result}.')
 
-    # Challenge 4.2, tuple output: ('Cox', 'Xoc', 3)
+    if run_choice == 4:
+        # Challenge 4, complete sort() function
+        Stream(names).source() \
+            .slice(8) \
+            .print('unsorted: ') \
+            .sort() \
+            .print('  sorted: ')
 
-    # Challenge 5, Pipeline for product codes
+    alpha_comperator = lambda n1, n2 : -1 if n1 < n2 else 1
+    len_alpha_comperator = lambda n1, n2 : -1 if len(n1) < len(n2) else 1 if len(n1) > len(n2) else alpha_comperator(n1, n2)
+    #
+    if run_choice == 41:
+        # Challenge 4.1, len-alpha comperator
+        Stream(names).source() \
+            .sort(len_alpha_comperator) \
+            .print('sorted: ')
 
-    # Challenge 5.1, even digit codes
+    if run_choice == 42:
+        # Challenge 4.2, tuple output: ('Cox', 'Xoc', 3)
+        result = Stream(names).source() \
+            .sort(len_alpha_comperator) \
+            .map(lambda n : (n, n[::-1].capitalize(), len(n))) \
+            .filter(lambda n1 : n1[2] % 2 == 1) \
+            .print('sorted: ') \
+            .count()
+        #
+        print(f'\\\\\n{result} odd-length names found.')
+
+    # rand_numbers = [random.randint(100000,999999) for i in range(30)]
+    # print(f'random numbers: {rand_numbers}')
+    #
+    if run_choice == 5 or run_choice == 51:
+        # Challenge 5, Pipeline for product codes
+        # Challenge 5.1, even digit codes
+        #
+        for i in range(1, 5):
+            # Stream of 5 random numbers from integer range, feel free to change
+            codes = Stream([random.randint(100000,999999) for j in range(1000)]).source() \
+                .filter(lambda n : n % 2 == 0) \
+                .cond( run_choice == 51, \
+                    # use only numbers with even digits, test by split up number in sequence of digits
+                    lambda op : op.filter(lambda n : len(set(map(int, str(n))).intersection([1, 3, 5, 7, 9])) == 0) \
+                ) \
+                .slice(5) \
+                .sort() \
+                .map(lambda n : f'X{n}-{sum(list(map(int, str(n)))) % 10}') \
+                .get()
+            #
+            print(f'batch {i}: {codes}')
